74c6472118f90de768c36f4f390dff7d6a76e2a7
Grzojda <grzesiu2203@gmail.com>
2021-06-18 15:12:05 +0200

    Update main.js
    Fix missing ')'
    Added attempt counter

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
+attempts = 0;
 jQuery( document ).ready(function(){
 jQuery('#writeInput').keyup(function(){
   //if Your accuracy isn't equal to 0(start point) or 100
   if(parseFloat(jQuery('#stats').children().eq(0).children().eq(1).text()) != 0.00 && parseFloat(jQuery('#stats').children().eq(0).children().eq(1).text()) != 100.00)
    {
-   // Show message
-   alert("Your accuracy went under 100%. I'm restarting now ;)");
+	 //add attemp
+   attempts++;
+	 // Show message
+   alert("Your accuracy went under 100%.\nThis was Your " + attempts + ". attempt.\nI'm restarting now ;)");
    // Click restart button
-   $('#configurations').children().eq(2).click()
+   $('#configurations').children().eq(2).click();
    }
 });
-};
+});
